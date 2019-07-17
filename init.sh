b5f9347857681074c8fac3372f62ff0c75f4b45f
Mauro Sardara <msardara@cisco.com>
2019-07-17 18:12:50 +0200

    Fix Dockerfile and init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/hicn/opendaylight-0.9.2/bin/start &
+sleep 10
+/hicn/opendaylight-0.9.2/bin/client feature:install \
+                                    odl-netconf-all \
+                                    odl-netconf-connector \
+                                    odl-restconf-all \
+                                    odl-netconf-topology
+
+wait
